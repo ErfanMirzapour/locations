15af766f343e1d101b56acdca6df2f8648d24ced
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2021-05-25 18:33:32 +0430

    Set up react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+   <QueryClientProvider client={queryClient}>
+      <App />
+   </QueryClientProvider>,
+   document.getElementById('root')
+);
