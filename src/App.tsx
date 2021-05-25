8c3cb7c61c12167c8551d71eac6a0143f0eaec1e
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2021-05-26 03:43:14 +0430

    Lift userId state up in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+// import { useEffect } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
-import { Locations } from 'components';
+import { Locations, AddLocation } from 'components';
+import {  useSessionStorage } from 'hooks';
 
-function App() {
+const App = () => {
+   const [userId, setUserId] = useSessionStorage<number>('id');
+   // const { mutate } = useDeleteLocation();
+
+   // Delete user data after closing the tab
+   // useEffect(() => {
+   //    const beforeUnload = () => {
+   //       if (userId) mutate({ userId });
+   //    };
+
+   //    window.addEventListener('beforeunload', beforeUnload);
+
+   //    return () => window.removeEventListener('beforeunload', beforeUnload);
+   // }, [mutate, userId]);
+
    return (
       <MapContainer
          center={[42.1634034, 21.4453125]}
@@ -13,9 +29,11 @@ function App() {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
          />
-         <Locations />
+
+         <Locations userId={userId} />
+         <AddLocation userId={userId} setUserId={setUserId} />
       </MapContainer>
    );
-}
+};
 
 export default App;
