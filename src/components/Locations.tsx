fc37820dc59e0da55d1283da92aa71b610c21577
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2021-05-26 03:43:57 +0430

    Render others' locations differently

--- src/components/Locations.tsx
+++ src/components/Locations.tsx
@@ -1,18 +1,28 @@
-import { useLocationsQuery } from 'hooks';
+import { memo } from 'react';
 import { Marker, Popup } from 'react-leaflet';
 
-const Locations = () => {
+import { useLocationsQuery } from 'hooks';
+
+interface Props {
+   userId: number;
+}
+
+const Locations = memo(({ userId }: Props) => {
    const { data: locations } = useLocationsQuery();
 
    return (
       <>
          {locations?.map(({ coords, id, name }) => (
-            <Marker key={id} position={[coords.lat, coords.lng]}>
+            <Marker
+               key={id}
+               position={[coords.lat, coords.lng]}
+               opacity={id === userId ? 1 : 0.6}
+            >
                <Popup>{name}</Popup>
             </Marker>
          ))}
       </>
    );
-};
+});
 
 export default Locations;
