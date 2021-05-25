30df8f9ca184f7c3bd39eba7fb81eb3d71a828e6
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2021-05-25 18:49:45 +0430

    Set up basic map

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { MapContainer, TileLayer } from 'react-leaflet';
+
 function App() {
-  return (
-    <>
-    </>
-  );
+   return (
+      <MapContainer
+         center={[42.1634034, 21.4453125]}
+         zoom={4}
+         scrollWheelZoom={true}
+      >
+         <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+         />
+      </MapContainer>
+   );
 }
 
 export default App;
