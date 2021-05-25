4231f84e8407197a73fe01d977c1b4de37f990bd
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2021-05-26 03:41:23 +0430

    Fix and refactor useSessionStorage

--- src/hooks/useSessionStorage.ts
+++ src/hooks/useSessionStorage.ts
@@ -1,24 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const useSessionStorage = <T>(key: string, init: T | (() => T)) => {
-   const persist = (newState: T) => {
+const parse = (str: string | null = null) => {
+   try {
+      return JSON.parse(str!);
+   } catch {
+      return str;
+   }
+};
+
+const useSessionStorage = <T>(key: string, init?: T | (() => T)) => {
+   const persist = (newState?: T) => {
       const serializedState = JSON.stringify(newState);
 
       sessionStorage?.setItem(key, serializedState);
    };
 
-   const [state, setState] = useState(() => {
+   const [state, setState] = useState<T>(() => {
       const initialize = init instanceof Function ? init() : init;
 
       if (sessionStorage) {
-         let storedInit = JSON.parse(sessionStorage.getItem(key)!);
+         let storedInit = parse(sessionStorage.getItem(key));
 
          if (storedInit) return storedInit;
 
-         persist(initialize);
+         if (initialize) persist(initialize);
       }
       return initialize;
    });
+
+   useEffect(() => {
+      const onStorageChange = (e: StorageEvent) => {
+         if (e.storageArea === sessionStorage && e.key === key)
+            setState(e.newValue as any);
+      };
+
+      window.addEventListener('storage', onStorageChange, { capture: true });
+
+      return () => window.removeEventListener('storage', onStorageChange);
+   }, [key]);
 
    const setSessionState: React.Dispatch<React.SetStateAction<T>> =
       newState => {
